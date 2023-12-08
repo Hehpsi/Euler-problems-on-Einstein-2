a51ad2c821eade33e0f2a651e2721f958f37b499
Hephzibah Victor Bode-Favours <hephzibah.bodefavours2@mail.dcu.ie>
2023-12-08 13:15:58 +0000

    Add new file

---
+++ problems/euler-100-arranged-probability.pl/euler-100-arranged-probability.pl
@@ -0,0 +1,16 @@
+% Base case to stop the recursion
+b(B, N, L) :-
+    N > L,
+    write(''), write(B), nl,
+    !.  % Cut to prevent backtracking
+
+% Recursive case to calculate the next values
+b(B, N, L) :-
+    N =< L,
+    NewB is 3*B + 2*N - 2,
+    NewN is 4*B + 3*N - 3,
+    b(NewB, NewN, L).
+
+% Start the process with initial values for B and N, and a limit L
+start(B, N, L) :-
+    b(B, N, L).
